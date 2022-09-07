9efaa6a24cdde33ca71ef4b60e724e8c0918c8e8
Antoine Robert <antoine.robert7700@gmail.com>
2022-09-07 17:03:04 +0200

    Refactor rooms.findUnique.service

--- src/services/rooms/rooms.findUnique.service.ts
+++ src/services/rooms/rooms.findUnique.service.ts
@@ -2,10 +2,10 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const findRoomWhereId = async ( id:string ) => {
+const findRoomWhereId = async ( ro_id:string ) => {
     return await prisma.rooms.findUnique({
         where: {
-            ro_id: id
+            ro_id
         }
     })
 }
