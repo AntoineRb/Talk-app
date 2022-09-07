9f5ccd3f361d0933cfff1ddb1f35a31d2da26535
Antoine Robert <antoine.robert7700@gmail.com>
2022-09-07 17:03:29 +0200

    Refactor users.delete.service

--- src/services/users/users.delete.service.ts
+++ src/services/users/users.delete.service.ts
@@ -2,10 +2,10 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const deleteUser = async ( id:string ) => {
+const deleteUser = async ( user_id:string ) => {
     return await prisma.users.delete({
         where: {
-            user_id: id
+            user_id
         }
     })
 }
