c782826aa979a812a2c35c9af9144387af744283
Antoine Robert <antoine.robert7700@gmail.com>
2022-09-08 20:37:34 +0200

    Refactor code presentation

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,17 @@
 import Express from "express";
-import http from "http";
 import { Server } from "socket.io";
 import { ExpressPeerServer } from "peer";
-
-import path from "path";
-
 import cookieParser from "cookie-parser";
-
+import path from "path";
+import http from "http";
 import config from "./config";
-
 import registerRoutes from "@/routes/user/register.routes";
-
 import authMiddleware from "@/middlewares/jwt/jwt.validate.middleware"
-
 
 const app = Express();
 const server = new http.Server( app );
 const io = new Server( server );
-
 const peerServer = ExpressPeerServer( server );
-
 const PORT = config.port;
 
 app
@@ -31,17 +23,11 @@ app
     .use( cookieParser() );
     
 app 
-    .get('/ping', ( req , res )  => {
-        res.send("ping")
-    })
-    .use( registerRoutes )
-    .use( authMiddleware )
-    // .get('/pong', authMiddleware, ( req , res )  => {
-    //     res.send("pong")
-    // })
     .get('/', ( req , res )  => {
         res.render('index')
-    });
+    })
+    .use( registerRoutes )
+    .use( authMiddleware );
 
 server.listen( PORT, () => {
     console.log( `Server listening on: http://localhost:${PORT}/` )
