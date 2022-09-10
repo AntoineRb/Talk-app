6de34f9148265030d584cf46493c604fe0fdda4b
Antoine Robert <antoine.robert7700@gmail.com>
2022-09-10 16:22:44 +0200

    Change port in server config to match with public config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    port: process.env.PORT || "4004",
+    port: process.env.PORT || "8000",
     rsaPrivateKeyPath: process.env.PRODUCTION ? 'Insert production key path here...' : __dirname.replace('src', 'rsa.private.key'),
     rsaPublicKeyPath: process.env.PRODUCTION ? 'Insert production key path here...' : __dirname.replace('src', 'rsa.pub.key'),
 }
