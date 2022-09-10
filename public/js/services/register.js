c1e5257c126daf7ae43a6cf6bcfa6d42a89529ef
Antoine Robert <antoine.robert7700@gmail.com>
2022-09-10 11:12:25 +0200

    Init register input event and check points

--- public/js/services/register.js
+++ public/js/services/register.js
@@ -1,12 +1,26 @@
 import config from "./config.js"
+import { emailRegex, passwordRegex, usernameRegex } from "../utils/regex.js";
 
 const registerForm = document.querySelector('.register-form');
 
 registerForm.addEventListener('submit', async ( e ) => {
     e.preventDefault();
-    const email = registerForm.querySelector('.email').value;
-    const username = registerForm.querySelector('.username').value;
-    const password = registerForm.querySelector('.password').value;
+    const email = registerForm.querySelector('#email').value;
+    const username = registerForm.querySelector('#username').value;
+    const password = registerForm.querySelector('#password').value;
+    const passwordConfirm = registerForm.querySelector('#password-confirmation').value;
+    if ( !emailRegex.test( email ) ) {
+        // do something...
+    }
+    if ( !usernameRegex.test( user ) ) {
+        // do something...
+    }
+    if ( !passwordRegex.test( password ) ) {
+        // do something...
+    }
+    if ( password !== passwordConfirm ) {
+        // do something...
+    }
     await fetch( config.BASE_URL + '/register', {
         method: 'POST',
         headers: {
